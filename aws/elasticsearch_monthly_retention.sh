629bfdc0598130219fbf5baea12eefc7c4b9738b
Your Name <you@example.com>
2017-04-21 10:20:08 +0530

    added documentation

--- aws/elasticsearch_monthly_retention.sh
+++ aws/elasticsearch_monthly_retention.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 Read -p "Enter Elasticsearch url" url
-
-index=cwl-$(date --date="30 day ago" +%Y.%m.%d)
+#How many days do you want to keep the logs
+Read -p "Enter the offset date" dateOffset
+# AWS Lambda normally names the index as cwl-17.04.20
+index=cwl-$(date --date="$dateOffset day ago" +%Y.%m.%d)
 curl -XDELETE $url/$index"
